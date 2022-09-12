99ec8deac577a273c5ef27026dbae70dfca2c36c
Joelle Zane <Joellezane@hotmail.com>
2022-09-12 17:52:16 +0100

    created Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">  
-      </header>
-      Hello
-      <div className="btn btn-light">Test</div>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className=" App-footer">
+          <small> Open-source code by Joelle Zane </small>
+        </footer>
+      </div>
     </div>
   );
 }
