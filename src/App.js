68af438b66d4c38eb446b996c82430544ad5f9a2
Joelle Zane <Joellezane@hotmail.com>
2022-09-13 17:30:38 +0100

    added some styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header">Joelle's Dictionary</header>
         <main>
           <Dictionary />
         </main>
