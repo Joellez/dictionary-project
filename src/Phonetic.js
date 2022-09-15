01f676bcd8fdf0b8a54af0a9fc6a59086f0f115d
Joelle Zane <Joellezane@hotmail.com>
2022-09-15 14:27:32 +0100

    update phonetic.js

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,14 +1,11 @@
 import React from "react";
 
 export default function Phonetic(props) {
-    function playAudio(){
-
-    }
     
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        <i class="fa-solid fa-volume-high" title="Listen" onClick={playAudio}></i>
+        <i class="fa-solid fa-volume-high" title="Listen"></i>
       </a> {props.phonetic.text}
     </div>
   );
