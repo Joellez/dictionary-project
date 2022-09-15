ff1c6fdbed529ca60029d3d0f2743bc87a762804
Joelle Zane <Joellezane@hotmail.com>
2022-09-15 14:50:13 +0100

    added external audio component

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,9 +1,17 @@
 import React from "react";
+import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetic(props) {
     
   return (
     <div className="Phonetic">
+        <div className="audio-player">
+          <ReactAudioPlayer
+            src={props.phonetic.audio}
+            autoPlay={false}
+            controls
+          />
+          </div>
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         <i class="fa-solid fa-volume-high" title="Listen"></i>
       </a> {props.phonetic.text}
